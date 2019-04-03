65ae2ed2d08c5fc31a8f4e5799107b98cf706856
Remy Gomes <remy.gomes@cosmosoftware.io>
2019-04-03 10:02:59 +0800

    :zap: kite-common package

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const TestUtils = require('./util/TestUtils');
+const WebDriverFactory = require('./wdmgmt/WebDriverFactory');
+const WebDriverUtility = require('./wdmgmt/WebDriverUtility');
+
+
+exports.TestUtils = TestUtils;
+exports.WebDriverFactory = WebDriverFactory;
+exports.WebDriverUtility = WebDriverUtility;
