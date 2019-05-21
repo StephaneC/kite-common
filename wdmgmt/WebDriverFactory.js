42d2fa9d9266c1f89afee1a34f327b5efd6eb491
Remy Gomes <remy.gomes@cosmosoftware.io>
2019-05-21 15:53:08 +0800

    Code style

--- wdmgmt/WebDriverFactory.js
+++ wdmgmt/WebDriverFactory.js
@@ -1,36 +1,35 @@
 const WebDriverUtils = require('./WebDriverUtility.js');
 const {Builder} = require('selenium-webdriver');
 module.exports = {
-	getDriver: async function(capabilities, remoteAddress) {
-		//to make sure the cap doesn't has anything weird:
-		const cap = {};
-		cap.browserName = capabilities.browserName;
-		cap.version = capabilities.version;
-		cap.platformName = capabilities.platformName;
-		cap.platform = capabilities.platform;
+  getDriver: async function(capabilities, remoteAddress) {
+    //to make sure the cap doesn't has anything weird:
+    const cap = {};
+    cap.browserName = capabilities.browserName;
+    cap.version = capabilities.version;
+    cap.platformName = capabilities.platformName;
+    cap.platform = capabilities.platform;
 
-		const options =  WebDriverUtils.getOptions(cap);
-		switch (cap.browserName) {
-			case 'chrome': {
-				cap['goog:chromeOptions'] =  options;
+    const options =  WebDriverUtils.getOptions(cap);
+    switch (cap.browserName) {
+      case 'chrome': {
+        cap['goog:chromeOptions'] =  options;
         return new Builder()
-					.forBrowser('chrome')
-					.usingServer(remoteAddress)
-					.withCapabilities(cap)
-					.build();
-			}
-			case 'firefox': {
-				cap.acceptInsecureCerts = true;
+          .forBrowser('chrome')
+          .usingServer(remoteAddress)
+          .withCapabilities(cap)
+          .build();
+      }
+      case 'firefox': {
+        cap.acceptInsecureCerts = true;
         return new Builder()
-					.forBrowser('firefox')
-					.usingServer(remoteAddress)
-					.withCapabilities(cap)
-					.setFirefoxOptions(options)
-					.build();
-			}
-			default:
-				throw new Error('Unsupported browser type: ' + browserName);
-		}
-
-	}
+          .forBrowser('firefox')
+          .usingServer(remoteAddress)
+          .withCapabilities(cap)
+          .setFirefoxOptions(options)
+          .build();
+      }
+      default:
+        throw new Error('Unsupported browser type: ' + browserName);
+    }
+  }
 }
--- wdmgmt/WebDriverUtility.js
+++ wdmgmt/WebDriverUtility.js
@@ -1,50 +1,50 @@
 const firefox = require('selenium-webdriver/firefox');
 
 const getFirefoxOptions = function(browser) {
-	let firefoxOptions;
-	let path = process.env.KITE_HOME.split("\\").join('/');
-	let profile = path + "/third_party/";
-	if (profile != undefined) {
-		switch(browser.platform) {
-			case "WINDOWS": {
-				profile += "firefox-h264-profiles/h264-windows";
-				break;
-			}
-			case "MAC": {
-				profile += "firefox-h264-profiles/h264-mac";
-				break;
-			}
-			case "LINUS": {
-				profile += "firefox-h264-profiles/h264-linux";
-				break;
-			}
-		}
-		firefoxOptions = new firefox.Options().setProfile('C:/newGitHub/KITE-2.0/third_party/firefox-h264-profiles/h264-windows');
-	} else {
-		console.log("FIREFOX: Some tests require specific profile for firefox to work properly.");
-		firefoxOptions = new firefox.Options();	
-	}
-	firefoxOptions.setPreference("media.navigator.streams.fake", true);
+  let firefoxOptions;
+  let path = process.env.KITE_HOME.split("\\").join('/');
+  let profile = path + "/third_party/";
+  if (profile != undefined) {
+    switch(browser.platform) {
+      case "WINDOWS": {
+        profile += "firefox-h264-profiles/h264-windows";
+        break;
+      }
+      case "MAC": {
+        profile += "firefox-h264-profiles/h264-mac";
+        break;
+      }
+      case "LINUS": {
+        profile += "firefox-h264-profiles/h264-linux";
+        break;
+      }
+    }
+    firefoxOptions = new firefox.Options().setProfile('C:/newGitHub/KITE-2.0/third_party/firefox-h264-profiles/h264-windows');
+  } else {
+    console.log("FIREFOX: Some tests require specific profile for firefox to work properly.");
+    firefoxOptions = new firefox.Options();	
+  }
+  firefoxOptions.setPreference("media.navigator.streams.fake", true);
 
-	return firefoxOptions;
+  return firefoxOptions;
 }
 
 
 module.exports = {
-	getOptions: function(browser) {
-		switch(browser.browserName) {
-		  case 'chrome': {
-				const chromeOptions = {
-					'args': ['--use-fake-device-for-media-stream', '--use-fake-ui-for-media-stream']
-				};
-				return chromeOptions;	
-			}
-			case 'firefox': {
-				return getFirefoxOptions(browser);
-			}
-		  default:
-		    //todo
-		    return null;
-		}
-	}
+  getOptions: function(browser) {
+    switch(browser.browserName) {
+      case 'chrome': {
+        const chromeOptions = {
+          'args': ['--use-fake-device-for-media-stream', '--use-fake-ui-for-media-stream']
+        };
+        return chromeOptions;	
+      }
+      case 'firefox': {
+        return getFirefoxOptions(browser);
+      }
+      default:
+        //todo
+        return null;
+    }
+  }
 }
